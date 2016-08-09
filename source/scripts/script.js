7c75cefd796cc1592d8f7f6ad7d87fd7f2e6a498
Zoya Farid <Zoya Farid>
2016-08-09 11:09:21 +0500

    zoya first commit

---
+++ source/scripts/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+	$('#header .fa-bars').click(function(){
+		$('.navigation').slideToggle(1000);
+	});
+});
+// <i class="fa fa-times" aria-hidden="true"></i>
+
+
+// $(function(){
+// 	var fitnessheight = $('.fitness-article').outerHeight(true);
+// 	$('.fitness-figure').css('height', fitnessheight);
+
+// 	var bodyheight = $('.body').outerHeight(true);
+// 	$('.yours').css('height', bodyheight);
+
+// 	var walkheight = $('.walk img').outerHeight(true);
+// 	$('.beach-view').css('height', walkheight);
+
+// 	var secondheight = $('.life-style').outerHeight(true);
+// 	$('.fun').css('height', secondheight);
+
+// 	var fourthheight = $('.habits').outerHeight(true);
+// 	$('.involved').css('height', fourthheight);
+
+// });
+
+// $(window).resize(function() {
+// 	var fitnessheight = $('.fitness-article').outerHeight(true);
+// 	$('.fitness-figure').css('height', fitnessheight);
+
+// 	var bodyheight = $('.body').outerHeight(true);
+// 	$('.yours').css('height', bodyheight);
+
+// 	var walkheight = $('.walk img').outerHeight(true);
+// 	$('.beach-view').css('height', walkheight);
+
+// 	var secondheight = $('.life-style').outerHeight(true);
+// 	$('.fun').css('height', secondheight);
+
+// 	var fourthheight = $('.habits').outerHeight(true);
+// 	$('.involved').css('height', fourthheight);
+
+
+// });
